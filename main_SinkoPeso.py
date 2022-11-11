03b2a44cf98bc589eaae89bca98a812fc259ce06
BrendaSaucedo12 <A00829855@itesm.mx>
2022-11-11 13:46:53 -0600

    add info and libraries

---
+++ main_SinkoPeso.py
@@ -0,0 +1,12 @@
+'''
+Equipo Sinko Peso
+- Ricardo Hernández Rincón          | A00831818
+- Eduardo Andrés Valerin Vijil      | A00830774
+- Santiago Alejandro Minga Martínez | A00830698
+- Julio Eduardo Arvizu Castillo     | A00831346
+- Eunice Santos Galindo             | A00831991
+- Brenda Elena Saucedo González     | A00829855
+'''
+import agentpy as ap
+import matplotlib.pyplot as plt
+import seaborn as sns
